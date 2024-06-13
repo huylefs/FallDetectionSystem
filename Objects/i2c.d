10ebd49986e3d964930deb55bf1d3a3d896f48b6
huylefs <21020914@vnu.edu.vn>
2024-06-13 16:41:51 +0700

    Delete Objects directory

--- Objects/i2c.d
+++
@@ -1,8 +0,0 @@
-.\objects\i2c.o: I2C.c
-.\objects\i2c.o: .\RTE\Device\MKL46Z256VLL4\MKL46Z4.h
-.\objects\i2c.o: C:\Users\ADMIN\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\core_cm0plus.h
-.\objects\i2c.o: C:\Keil_v5\ARM\Compilerv506\Bin\..\include\stdint.h
-.\objects\i2c.o: C:\Users\ADMIN\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_version.h
-.\objects\i2c.o: C:\Users\ADMIN\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_compiler.h
-.\objects\i2c.o: C:\Users\ADMIN\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_armcc.h
-.\objects\i2c.o: .\RTE\Device\MKL46Z256VLL4\system_MKL46Z4.h
